f1d1e0c1f847acd69fc28a668d5e2f0c43a969f5
Anya <xxxuko@gmail.com>
2024-05-10 14:52:00 +0300

    update response

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
-from config import API_KEY, BASE_URL, LANGUAGE
+from datetime import datetime
+from config import API_KEY, BASE_URL, LANGUAGE, WEATHER_CONDITION
 
 
 def get_weather(city):
@@ -13,6 +14,7 @@ def get_weather(city):
             country_name = data["location"]["country"]
             city_name = data["location"]["name"]
             localtime = data["location"]["localtime"]
+            weather_description = data["current"]["condition"]["text"]
             temp_c = data["current"]["temp_c"]
             feels_like_c = data["current"]["feelslike_c"]
             temp_f = data["current"]["temp_f"]
@@ -20,13 +22,15 @@ def get_weather(city):
             wind_speed = data["current"]["wind_kph"]
             humidity = data["current"]["humidity"]
 
-            print(f"🌆 City: {city_name},\n"
-                  f"🌎 Country: {country_name},\n"
-                  f"⌚️ Localtime: {localtime},\n"
+            print(f"***{datetime.now().strftime("%Y-%m-%d %H:%M")}***,\n"
+                  f"Country/City: {country_name},{city_name},\n"
+                  f"Localtime: {localtime},\n\n"
+                  f"Weather condition: {WEATHER_CONDITION.get(weather_description, weather_description)},\n"
                   f"🌡 Temperature C: {temp_c}, (feels like: {feels_like_c}),\n"
-                  f"🌡 Temperature F: {temp_f}, (feels like: {feels_like_f}),\n"
-                  f"💨 Wind Speed: {wind_speed} kph,\n"
-                  f"💧 Humidity: {humidity},\n")
+                  f"🌡 Temperature F: {temp_f}, (feels like: {feels_like_f}),\n\n"
+                  f"Wind Speed: {wind_speed} kilometer per hour,\n"
+                  f"Humidity: {humidity},\n\n"
+                  f"😃 Have a Great Day!\n")
 
     except Exception as error:
         print(error)
