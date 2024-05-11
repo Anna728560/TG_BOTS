d79b744e2f2db7ece2cec71bae42a7f56ceeb529
Anya <xxxuko@gmail.com>
2024-05-11 17:07:06 +0300

    fix tables in models.py

--- shop_bot/database/models.py
+++ shop_bot/database/models.py
@@ -15,7 +15,7 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    tg_id: mapped_column(BigInteger, nullable=False)
+    tg_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
 
 
 class Category(Base):
