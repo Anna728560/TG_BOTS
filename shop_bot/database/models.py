af40b434302009998bcdd8de5858fe5283b4ccc0
Anya <xxxuko@gmail.com>
2024-05-11 16:06:19 +0300

    setup sqlalchemy database(create base class Base and tables User, Item, Category)

---
+++ shop_bot/database/models.py
@@ -0,0 +1,40 @@
+from sqlalchemy import BigInteger, String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+
+engine = create_async_engine(url="sqlite+aiosqlite:///db.sqlite3")
+async_session = async_sessionmaker(engine)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    tg_id: mapped_column(BigInteger, nullable=False)
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50))
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50))
+    description: Mapped[str] = mapped_column(String(100))
+    price: Mapped[str] = mapped_column()
+    category: Mapped[int] = mapped_column(ForeignKey("categories.id"))
+
+
+async def async_main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
