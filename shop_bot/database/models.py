ff735bf0884e3919db4f3589eb7c9c64905afe3c
Anya <xxxuko@gmail.com>
2024-05-11 18:40:17 +0300

    create json files with data about categories and items and create fill_database function

--- shop_bot/database/models.py
+++ shop_bot/database/models.py
@@ -1,3 +1,6 @@
+import json
+
+import aiofiles
 from sqlalchemy import BigInteger, String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
@@ -35,6 +38,45 @@ class Item(Base):
     category: Mapped[int] = mapped_column(ForeignKey("categories.id"))
 
 
+async def fill_database():
+    async with async_session() as session:
+        async with aiofiles.open(
+                file="categories.json",
+                mode="r",
+                encoding="utf-8"
+        ) as file:
+            categories = json.loads(await file.read())
+
+            for category_data in categories:
+                category = Category(
+                    id=category_data["pk"],
+                    name=category_data["name"]
+                )
+
+                session.add(category)
+            await session.commit()
+
+        async with aiofiles.open(
+                file="items.json",
+                mode="r",
+                encoding="utf-8"
+        ) as file:
+            items = json.loads(await file.read())
+
+            for item_data in items:
+                item = Item(
+                    id=item_data["pk"],
+                    name=item_data["name"],
+                    description=item_data["description"],
+                    price=item_data["price"],
+                    category=int(item_data["category"])
+                )
+
+                session.add(item)
+            await session.commit()
+
+
 async def async_main():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+        await fill_database()
