ccf61d445acbe2401eb4ef3e74519833ec6d54ee
Anya <xxxuko@gmail.com>
2024-05-12 22:59:09 +0300

    update item_details

--- shop_bot/bot_config/keyboards.py
+++ shop_bot/bot_config/keyboards.py
@@ -29,11 +29,11 @@ async def inline_items(category_id: int):
     return keyboard.adjust(2).as_markup()
 
 
-async def item_details():
+async def item_details(item_id: int):
     keyboard = InlineKeyboardBuilder()
     keyboard.add(InlineKeyboardButton(
         text="I'm buying!📦",
-        callback_data="to_pay")
+        callback_data=f"to_pay_item_{item_id}")
     )
     keyboard.add(InlineKeyboardButton(
         text="Back",
